7e6a905b4840e14fe63d4b39ec91eaed1fcca6fd
Devin Fiachra <devinduffymeade@gmail.com>
2023-06-09 18:28:45 +0200

    set up game menu navigation logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("Hello, World!");
+// Screens
+
+const startScreen = document.getElementById("start-screen");
+const customGameScreen = document.getElementById("custom-game-menu-screen");
+const gameScreen = document.getElementById("game-screen");
+const endGameScreen = document.getElementById("end-game-screen");
+
+// START MENU
+
+const startButton = document.getElementById("start-game");
+const optionsMenu = document.getElementById("open-options");
+
+startButton.addEventListener("click", (e) => {
+  alert("START CLASSIC GAME");
+  startScreen.style.visibility = "hidden";
+  gameScreen.style.visibility = "visible";
+});
+
+optionsMenu.addEventListener("click", (e) => {
+  alert("CUSTOM GAME MENU");
+  startScreen.style.visibility = "hidden";
+  customGameScreen.style.visibility = "visible";
+});
+
+// CUSTOM GAME MENU
